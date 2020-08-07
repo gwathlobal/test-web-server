36d3b8ba7f8c150b039a70d5832cb86632beebaf
gwathlobal <gwathlobal@yandex.ru>
2020-08-07 20:54:40 +0300

    First lesson done

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,26 @@
+package Main;
+
+import Servlets.AllRequestsServlet;
+import Servlets.MirrorServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        MirrorServlet mirrorServlet = new MirrorServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(mirrorServlet), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
+        //System.out.println("Server started");
+        server.join();
+    }
+}
---
+++ src/main/java/Servlets/MirrorServlet.java
@@ -0,0 +1,31 @@
+package Servlets;
+
+import Templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MirrorServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException
+    {
+        String value = request.getParameter("key");
+
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("key", (value == null) ? "" : value);
+        pageVariables.put("parameters", request.getParameterMap().toString());
+
+        //response.getWriter().println(PageGenerator.instance().getPage("mirror.html", pageVariables));
+        response.getWriter().println(value == null ? "" : value);
+
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
